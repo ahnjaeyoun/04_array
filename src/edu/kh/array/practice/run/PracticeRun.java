7b34d2050c87bc030dd9cc1789cc990668aef087
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-07-20 17:47:33 +0900

    aca

---
+++ src/edu/kh/array/practice/run/PracticeRun.java
@@ -0,0 +1,22 @@
+package edu.kh.array.practice.run;
+
+import edu.kh.array.practice.service.PracticeService;
+
+public class PracticeRun {
+	public static void main(String[] args) {
+		PracticeService pt = new PracticeService(); 
+			//pt.practice1();
+			pt.practice2();
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+	}
+
+}
